35bceb588ce6a162d92aaa4b90e921f38677cd26
Michael Trip <m.trip@atcomputing.nl>
2022-05-19 13:40:22 +0200

    added debug

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -6,6 +6,6 @@ swayidle \
     timeout 10 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -c 550000
+swaylock -c 550000 --debug
 # Kills last background task so idle timer doesn't keep running
 kill %%
